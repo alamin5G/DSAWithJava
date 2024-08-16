68ffb9b7e404d79ab9b1764364a49d003d44ade8
alamin5g <alaminrakib4864@gmail.com>
2024-08-16 13:54:33 +0600

    Object Oriented Design page 70 DSA in Java

---
+++ src/chapter_2/Progression.java
@@ -0,0 +1,42 @@
+package chapter_2;
+
+// generate a simple progression. By default 0, 1, 2...
+import java.util.Scanner;
+public class Progression {
+    //instance variable
+    protected long current;
+
+    //construct a progression starting at zero
+    public Progression() {
+        this.current = 0;
+    }
+
+    //construct a progression with given start value
+    public Progression(long start) {
+        this.current = start;
+    }
+
+    //return the next value of the progression
+    public long nexValue()
+    {
+        long temp = this.current;
+        advance();
+        return temp;
+    }
+
+    //advance the current value to the next value of the progression
+    protected void advance(){
+        current = current + 1;
+    }
+
+    //print the next n values of the progression separated by spaces
+    public void printProgression(int n){
+        System.out.print(nexValue());
+        for (int i = 1; i < n; i++) {
+            System.out.print(" " + nexValue());
+        }
+        System.out.println();
+    }
+
+
+}
